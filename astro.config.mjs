2edcbec07fa45c9b605c03953acb6d3de5acb800
espenraugstad <espen.raugstad@gmail.com>
2026-08-12 19:18:43 +0200

    Added action to deploy to gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+     site: 'https://espenraugstad.github.io',
+     base: '/astro-gh-pages',
+});
