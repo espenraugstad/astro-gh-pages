14a97ad2256caac9c5d2ed39d3964616d90915a0
espenraugstad <espen.raugstad@gmail.com>
2026-08-13 09:10:58 +0200

    Added link to test script on front page

--- scripts/hide-vg-header.user.js
+++ scripts/hide-vg-header.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Hide VG header
 // @namespace    http://tampermonkey.net/
-// @version      2026-08-13
+// @version      2026-08-13.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.vg.no/
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @updateURL    https://www.vg.no">the page VG</a>. You can <a href="https://github.com/espenraugstad/astro-gh-pages/raw/refs/heads/main/scripts/hide-vg-header.user.js
+// @downloadURL  https://www.vg.no">the page VG</a>. You can <a href="https://github.com/espenraugstad/astro-gh-pages/raw/refs/heads/main/scripts/hide-vg-header.user.js
 // ==/UserScript==
 
 (function() {
